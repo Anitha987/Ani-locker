149b9528a3fd29210da36a3680db8fea317bcefc
BlandineK <blandineuwase222@gmail.com>
2019-09-05 11:54:53 +0200

    add a init function

--- address-test.py
+++ address-test.py
@@ -1,2 +1,18 @@
 import unittest
 form address import Address
+
+ class TestAddress(unittest.TestCase):
+   """
+   Test class that defines test cases for the class of address.
+
+   Args:
+       unittest.TestCase:TestCase class that helps in creating test cases
+   """  
+  def setUp(self):
+   """ 
+   this setUp method will be run after each test cases.
+   """
+   self.new_address=Address("Anitha987","anithaumuhire@gmail.com","kigali555")
+  def test_init(self):
+    """
+    the  init above will test if  
