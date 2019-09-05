629cc1179614cf1aa5f4296885e54a3568876c9b
BlandineK <blandineuwase222@gmail.com>
2019-09-05 11:17:56 +0200

    add properties

--- address.py
+++ address.py
@@ -3,8 +3,9 @@ class Address:
  created a Class that will generate user's info
  """
  pass
- def_init_(self,username;e_mail,password_):
+ def_init_(self,username;email,password_):
    """
    _init_method which is going to help us define properties for all our objects.
    Args:
-   username:New address username
+   username:New address username.
+   email:New  address e-mail.
