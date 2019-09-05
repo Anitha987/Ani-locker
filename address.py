80bc220d852126cbdf1ffeace05f92c9ebc402c9
BlandineK <blandineuwase222@gmail.com>
2019-09-05 11:26:01 +0200

    add an empty array

--- address.py
+++ address.py
@@ -1,12 +1,12 @@
 class Address:
- """
- created a Class that will generate user's info
- """
- pass
- def_init_(self,username;email,password_):
-   """
-   _init_method which is going to help us define properties for all our objects.
-   Args:
-   username:New address username.
-   email:New  address e-mail.
-   password:New address password
+    """
+    created a Class that will generate user's info
+    """
+    address_list=[]
+    def_init_(self,username;email,password_):
+    """
+    _init_method which is going to help us define      properties for all our objects.
+     Args:
+     username:New address username.
+    email:New  address e-mail.
+    password:New address password.
