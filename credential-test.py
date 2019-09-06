e4fa0321a3103f02e48fe2f3e0277067d2c9889c
BlandineK <blandineuwase222@gmail.com>
2019-09-06 16:19:17 +0200

    add conditions

--- credential-test.py
+++ credential-test.py
@@ -1,14 +1,36 @@
-import unnitest
-from credential import credential
+import unittest
+from credential import Credential
 
-class TestCredential(unnitest.TestCase):
+class TestCredential(unittest.TestCase):
   """
   Test class that defines test cases for  the credential class behaviours
   Args:
       unittest.TestCase:TestCase class that helps in creating test cases
   """
   def setUp(self):
-      """ 
-      this setUp method will be run after each test cases.
-      """
-      self.new_credential=Credential("Anitha987","anithaumuhire@gmail.com","kigali555","twitter")
+    """ 
+    this setUp method will be run after each test cases.
+    """
+    self.new_credential= Credential("Anitha987","anithaumuhire@gmail.com","kigali555","twitter")
+  def test_init (self):
+    """
+    test_init testcase to test if the objects are initialised properly
+    """
+    self.assertEqual(self.new_credential.username,"Anitha987")
+    self.assertEqual(self.new_credential.email,"anithaumuhire@gmail.com")
+    self.assertEqual(self.new_credential.password,"kigali555")
+    self.assertEqual(self.new_credential.account,"twitter")
+  def test_save_credential(self):
+      '''
+      test_save_credential test case to test if the credential act object is saved into the credential_list
+      '''
+      self.new_credential.save_credential()
+      self.assertEqual(len(Credential.credential_list),1) 
+  def tearDown(self):
+      '''tearDown method will help us to clean up once  each test has been run
+      '''
+      Credential.credential_list=[]   
+if __name__ == '__main__':
+    unittest.main()
+
+         
--- credential.py
+++ credential.py
@@ -2,17 +2,23 @@ class Credential:
   """
   create a class credential  to hold our user accounts
   """
-  credential_list[]
-  def _init_(self,username,email,password,account)
-  """
-  _init_method will help us by the time  we want to use initialise all our objects 
-  """
-  self.username=username
-  self.email=email
-  self.password =password
-  self.account=account
+  credential_list=[]
+  def __init__(self,username,email,password,account):
+    """
+    _init_method will help us by the time  we want to use initialise all our objects 
+    Args:
+    """
+    self.username=username
+    self.email=email
+    self.password =password
+    self.account=account
   def save_credential(self):
       '''
       save credential method will help us to save credential objects into credential-list
       '''
       Credential.credential_list.append(self)
+  def delete_credential(self):
+      '''
+      delete_credential method deletes a saved credential in case we want to remove one from our credential_list
+      '''
+      Credential.credential_list.remove(self)     
