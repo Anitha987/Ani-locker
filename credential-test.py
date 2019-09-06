64fba0973c2cc8a27873f0ca7341d6c16e8dd468
BlandineK <blandineuwase222@gmail.com>
2019-09-06 15:19:14 +0200

    add objects

--- credential-test.py
+++ credential-test.py
@@ -7,4 +7,8 @@ class TestCredential(unnitest.TestCase):
   Args:
       unittest.TestCase:TestCase class that helps in creating test cases
   """
-  
+  def setUp(self):
+      """ 
+      this setUp method will be run after each test cases.
+      """
+      self.new_credential=Credential("Anitha987","anithaumuhire@gmail.com","kigali555","twitter")
