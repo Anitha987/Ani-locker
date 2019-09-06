670159a1b9e20880d15ffbdd68e39f8eeac2a66b
BlandineK <blandineuwase222@gmail.com>
2019-09-06 09:55:56 +0200

    add a pyperclip

--- address-test.py
+++ address-test.py
@@ -1,25 +1,75 @@
+import pyperclip
 import unittest
-form address import Address
+from address import Address
 
- class TestAddress(unittest.TestCase):
-   """
-   Test class that defines test cases for the class of address.
-
-   Args:
-       unittest.TestCase:TestCase class that helps in creating test cases
-   """  
-  def setUp(self):
-   """ 
-   this setUp method will be run after each test cases.
-   """
-   self.new_address=Address("Anitha987","anithaumuhire@gmail.com","kigali555")
-  def test_init(self):
+class TestAddress(unittest.TestCase):
     """
-    the  init above will test if our objects are initialized well
-    """"
-    self.assertEqual(self.new_Address username,"Anitha987")
-    self.assertEqual(self.new_Address email,"anithaumuhire@gmail.com")
-    self.assertEqual(self.new_Address username,"kigali555")
+    Test class that defines test cases for the class of address.
 
-    if_name_=='_main_':
-      unittest.main()
+    Args:
+        unittest.TestCase:TestCase class that helps in creating test cases
+    """  
+    def setUp(self):
+      """ 
+      this setUp method will be run after each test cases.
+      """
+      self.new_address=Address("Anitha987","anithaumuhire@gmail.com","kigali555")
+    def test_init(self):
+      """
+      the  init above will test if our objects are initialized well
+      """
+      self.assertEqual(self.new_address.username,"Anitha987")
+      self.assertEqual(self.new_address.email,"anithaumuhire@gmail.com")
+      self.assertEqual(self.new_address.password,"kigali555")
+    def test_save_address(self):
+      '''
+      test_save_contact test case to test case to test if the contact act object is saved into the address_list
+      '''
+      self.new_address.save_address()
+      self.assertEqual(len(Address.address_list),1)
+    def tearDown(self):
+        '''tearDown method will help us to clean up once  each test has been run
+        '''
+        Address.address_list=[]
+    def test_save_multiple_address(self):
+      '''
+      test_save_multiple_address to check if we can save multiple address objects to our address_list
+      '''
+      self.new_address.save_address()   
+      test_address=Address("Ronah","ronah@gmail.com","ronah123")
+      test_address.save_address()
+      self.assertEqual(len(Address.address_list),2)
+    def test_delete_address(self):
+      '''
+      test_delete_address help to test if in our address we can be able to remove one of our address list
+      '''
+      self.new_address.save_address()
+      test_address=Address("Ronah","ronah@gmail.com","ronah123")
+      test_address.save_address()
+      self.new_address.delete_address()
+      self.assertEqual(len(Address.address_list),1) 
+    def test_find_address(self):
+      '''
+      test_find_address help to testif we want to search one of our information
+      '''
+      self.new_address.save_address()
+      test_address=Address("Ronah","ronah@gmail.com","ronah123")
+      test_address.save_address()
+      found_address = Address.find_by_email("ronah@gmail.com")
+      self.assertEqual(found_address.email,test_address.email)
+    def test_address_exists(self):
+      '''
+      test and see bty the time our address is not found to return a boolean.
+      '''
+      self.new_address.save_address
+      test_address=Address("Ronah","ronah@gmail.com","ronah123")
+      test_address.save_address()
+      address_exists=Address.address_exist("ronah@gmail.com")
+      self.assertTrue(address_exists)  
+    def test_copy_email(self):
+      '''
+      Test to confirm that we are copying the email address  from a found address  
+      '''
+if __name__ == '__main__':
+    unittest.main()
+  
--- address.py
+++ address.py
@@ -3,10 +3,57 @@ class Address:
     created a Class that will generate user's info
     """
     address_list=[]
-    def_init_(self,username;email,password_):
-    """
+    def __init__(self,username,email,password):
+
+        '''
     _init_method which is going to help us define      properties for all our objects.
-     Args:
-     username:New address username.
-    email:New  address e-mail.
-    password:New address password.
+      Args:
+            
+        '''
+        self.username=username
+        self.email= email
+        self.password =  password
+    def save_address(self):
+      '''
+      save address method will help us to save address  objects into address-list
+      '''
+      Address.address_list.append(self)
+    def delete_address(self):
+      '''
+      delete_address method deletes asavedaddress in case we want to remove one from our address_list
+      '''
+      Address.address_list.remove(self)  
+    @classmethod
+    def find_by_email(cls,email):
+      '''
+      method that takes in an email and returns an address that matches that email.
+
+      Args:
+        email:email to search for
+      Returns: 
+        address of person that  matches the email
+      '''
+      for address in cls.address_list:
+        if address.email==email:
+          return address
+    @classmethod
+    def address_exist(cls,email):
+      '''
+      Method that checks if our entered email exists from the  address list.
+      Args:
+          email:email to search if it exists
+      Returns:
+        Boolean:True or false depending ifthe address provided exists
+      '''
+      for address in cls.address_list:
+        if address.email==email:
+          return True
+      return False  
+    @classmethod
+    def display_address(cls):
+      '''
+      method that returns our address list
+      '''
+      return cls.address_listy              
+ 
+           
