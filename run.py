eac73f2b53fbc3fe30ddae9271d9458e963933bd
BlandineK <blandineuwase222@gmail.com>
2019-09-06 17:17:39 +0200

    update readme.md

--- run.py
+++ run.py
@@ -1,2 +1,7 @@
 from address import address
   def create_address(username,email,password,account)
+  """
+  Function to  create a new contact
+  """
+  new_address=Address(username,email,password,account)
+  return new_
